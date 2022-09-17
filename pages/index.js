1c0171033b18572c3981172bdf114021e18dced9
Alex Panteli <alekspanteli@gmail.com>
2022-09-17 19:35:57 +0300

    add meta data and image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,7 +9,15 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Read <Link href="/posts/first-post">this page!</Link>
+      <p>
+        Read <Link href="/posts/first-post">this page!</Link>
+      </p>
+      <Image
+        src="/images/profile.webp" // Route of the image file
+        height={256} // Desired size with correct aspect ratio
+        width={256} // Desired size with correct aspect ratio
+        alt="Alex Panteli"
+      />
     </div>
   );
 }
