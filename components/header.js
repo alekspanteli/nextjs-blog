071b6f5613e09c20836db1a00fea3aed4ec54cbc
Alex Panteli <alekspanteli@gmail.com>
2022-09-18 18:38:44 +0300

    fix logo

--- components/header.js
+++ components/header.js
@@ -3,9 +3,9 @@ import Nav from "../components/nav";
 export default function Header() {
   return (
     <>
-      <header className="bg-[red] h-16">
-        <div className="wrapper h-full">
-          <div className="flex gap-3 justify-between items-center h-full items-center">
+      <header className="bg-[red] py-5">
+        <div className="wrapper">
+          <div className="flex gap-3 justify-between items-center items-center">
             <h1>Logo</h1>
             <Nav />
           </div>
