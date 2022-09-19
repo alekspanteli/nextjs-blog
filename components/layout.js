3548be5e949c161899d952abf6971a288acb47f1
Alex Panteli <alekspanteli@gmail.com>
2022-09-19 19:37:03 +0300

    test

--- components/layout.js
+++ components/layout.js
@@ -4,9 +4,6 @@ import Footer from "../components/footer";
 export default function Layout({ children }) {
   return (
     <div className="min-h-screen grid grid-rows-[auto_1fr_auto]">
-      <a href="#content" class="sr-only focus:not-sr-only">
-        Skip to content
-      </a>
       <Header />
       <main className="bg-[yellow]" id="content">
         <div className="wrapper">{children}</div>
